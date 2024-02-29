b73095c1dc1ce52d3517ce0a43a1edf4c90bc1e9
André Sousa <andrels9283@gmail.com>
2024-02-29 13:51:27 +0000

    tutorial done

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+	"time"
+)
+
+type Film struct {
+	Title    string
+	Director string
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		films := map[string][]Film{
+			"Films": {
+				{Title: "Casablanca", Director: "Michael Curtiz"},
+				{Title: "The Godfather", Director: "Francis Ford Coppola"},
+				{Title: "Raging Bull", Director: "Martin Scorsese"},
+			},
+		}
+
+		tmpl.Execute(w, films)
+	}
+
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1 * time.Second)
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
+
+	}
+
+	http.HandleFunc("/", h1)
+	http.HandleFunc("/add-film/", h2)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
